319fad3cbfd81ad636f18457efb8e3e4ee2314b1
jeong-1 <wjddms680@naver.com>
2024-04-17 19:51:35 +0900

    [TION-6] 🔧[INIT] Android 구조 세팅
    * TION-6 🔧 [INIT] Java 버전 17로 변경
    
    * TION-6 🔧[INIT] 코드 컨벤션 설정 및 gitignore 설정
    
    * TION-6 🔧[INIT] GitHub Actions를 위한 yaml 파일 작성
    
    * TION-6 🔧[INIT] 프로젝트 Dependency 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,13 @@
 plugins {
     id("com.android.application") version "8.2.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+
+    id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
+    id("com.diffplug.spotless") version "6.19.0"
+    id("com.dropbox.dependency-guard") version "0.3.2"
 }
+
+dependencyGuard {
+    configuration("classpath")
+}
+
