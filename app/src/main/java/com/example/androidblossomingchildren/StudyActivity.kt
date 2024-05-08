bd20572b0d94320b7b331cd07762e09864caf5c9
YUDINDIN1005 <109646304+YUDINDIN1005@users.noreply.github.com>
2024-05-08 15:17:22 +0900

    TION-21 ✨[FEAT] Study화면 UI 구현

--- app/src/main/java/com/example/androidblossomingchildren/StudyActivity.kt
+++ app/src/main/java/com/example/androidblossomingchildren/StudyActivity.kt
@@ -4,11 +4,16 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -18,6 +23,7 @@ import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -25,7 +31,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -33,11 +40,12 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.androidblossomingchildren.ui.theme.AndroidBlossomingChildrenTheme
 import com.example.androidblossomingchildren.ui.theme.Blue
+import com.example.androidblossomingchildren.ui.theme.Red
 
 class StudyActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent{
+        setContent {
             AndroidBlossomingChildrenTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -52,37 +60,96 @@ class StudyActivity : ComponentActivity() {
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun StudyScreen(activity : ComponentActivity) {
+fun StudyScreen(activity: ComponentActivity) {
     Scaffold(
         topBar = {
-            StudyTopAppBar(onBackClicked = {activity.finish()})
+            StudyTopAppBar(onBackClicked = { activity.finish() })
         },
-        content = {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(16.dp),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
+        content = { padding ->
+            val itemList = List(20) { "제목 ${it + 1}" }
+
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                contentPadding = PaddingValues(all = 16.dp),
+                modifier = Modifier.padding(padding),
             ) {
-                Text("여기는 동작 교육 화면입니다! 👏👏👏")
+                items(itemList) { item ->
+                    GridItem(item)
+                }
+            }
+        },
+    )
+}
+
+@Composable
+fun GridItem(item: String) {
+    val progress = 0.75f
+
+    Card(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxSize(),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 4.dp,
+        ),
+    ) {
+        Column(modifier = Modifier.padding(8.dp)) {
+            Image(
+                painter = painterResource(id = R.drawable.video_example),
+                contentDescription = item,
+                modifier = Modifier
+                    .height(100.dp)
+                    .fillMaxWidth(),
+                contentScale = ContentScale.Crop,
+            )
+            Spacer(Modifier.height(8.dp))
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth(),
+            ) {
+                LinearProgressIndicator(
+                    progress = progress,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f),
+                    color = Red,
+                    trackColor = Color.LightGray,
+                )
+                Spacer(Modifier.width(8.dp))
+
+                Text(
+                    text = "${(progress * 100).toInt()}%",
+                    style = MaterialTheme.typography.bodyMedium.copy(fontFamily = FontFamily(Font(R.font.laundrygothic_regular))),
+                )
+                Spacer(Modifier.height(8.dp))
+            }
+
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Icon(
+                    painter = painterResource(id = R.drawable.bookmark_blank),
+                    contentDescription = "Bookmark",
+                )
+                Spacer(Modifier.width(8.dp))
+                Text(
+                    text = item,
+                    style = MaterialTheme.typography.bodyLarge.copy(fontFamily = FontFamily(Font(R.font.laundrygothic_regular))),
+                )
             }
         }
-    )
+    }
 }
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun StudyTopAppBar(onBackClicked: () -> Unit) {
-    val backIcon : Painter = painterResource(id = R.drawable.back_arrow)
-
     CenterAlignedTopAppBar(
         navigationIcon = {
             IconButton(onClick = onBackClicked) {
                 Icon(
-                    painter = backIcon,
-                    contentDescription = "Back"
+                    painter = painterResource(id = R.drawable.back_arrow),
+                    contentDescription = "Back",
                 )
             }
         },
@@ -91,8 +158,8 @@ fun StudyTopAppBar(onBackClicked: () -> Unit) {
                 text = "동작 교육",
                 fontFamily = FontFamily(Font(R.font.laundrygothic_bold)),
                 color = Blue,
-                fontSize = 20.sp
+                fontSize = 20.sp,
             )
-        }
+        },
     )
 }
