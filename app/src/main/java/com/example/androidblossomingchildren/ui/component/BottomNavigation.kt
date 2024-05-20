d050bbb2274882a88ad9680c791dce6685fc794b
jeong-1 <wjddms680@naver.com>
2024-05-20 15:23:49 +0900

    TION-27 ✨[FEAT] 바텀 네비게이션  컴포넌트 생성

---
+++ app/src/main/java/com/example/androidblossomingchildren/ui/component/BottomNavigation.kt
@@ -0,0 +1,93 @@
+package com.example.androidblossomingchildren.ui.component
+
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import com.example.androidblossomingchildren.ui.ThemePreviews
+import com.example.androidblossomingchildren.ui.icon.TionIcons
+import com.example.androidblossomingchildren.ui.theme.TionTheme
+
+@Composable
+fun RowScope.TionNavigationBarItem(
+    selected: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    icon: @Composable () -> Unit,
+    selectedIcon: @Composable () -> Unit,
+) {
+    NavigationBarItem(
+        selected = selected,
+        onClick = onClick,
+        icon = if (selected) selectedIcon else icon,
+        modifier = modifier,
+        enabled = enabled,
+        colors =
+        NavigationBarItemDefaults.colors(
+            indicatorColor = MaterialTheme.colorScheme.onSecondaryContainer,
+            selectedIconColor = MaterialTheme.colorScheme.secondaryContainer,
+            unselectedIconColor = MaterialTheme.colorScheme.onSecondaryContainer,
+        ),
+    )
+}
+
+@Composable
+fun TionNavigationBar(
+    modifier: Modifier = Modifier,
+    content: @Composable RowScope.() -> Unit,
+) {
+    NavigationBar(
+        modifier = modifier,
+        containerColor = MaterialTheme.colorScheme.onTertiaryContainer,
+        contentColor = MaterialTheme.colorScheme.onTertiaryContainer,
+        content = content,
+    )
+}
+
+@ThemePreviews
+@Composable
+private fun TionNavigationBarPreview() {
+    val items = listOf("Home", "Stamp", "MyPage")
+    val icons =
+        listOf(
+            TionIcons.Home,
+            TionIcons.Stamp,
+            TionIcons.MyPage,
+        )
+
+    val selectedIcons =
+        listOf(
+            TionIcons.HomeSelected,
+            TionIcons.StampSelected,
+            TionIcons.MyPageSelected,
+        )
+
+    TionTheme {
+        TionNavigationBar {
+            items.forEachIndexed { index, _ ->
+                TionNavigationBarItem(
+                    selected = index == 1,
+                    onClick = { },
+                    icon = {
+                        Icon(
+                            painterResource(id = icons[index]),
+                            contentDescription = null,
+                        )
+                    },
+                    selectedIcon = {
+                        Icon(
+                            painterResource(id = selectedIcons[index]),
+                            contentDescription = null,
+                        )
+                    },
+                )
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/androidblossomingchildren/ui/icon/TionIcons.kt
@@ -0,0 +1,12 @@
+package com.example.androidblossomingchildren.ui.icon
+
+import com.example.androidblossomingchildren.R
+
+object TionIcons {
+    val Home = R.drawable.ic_home
+    val HomeSelected = R.drawable.ic_home_selected
+    val Stamp = R.drawable.ic_fire
+    val StampSelected = R.drawable.ic_fire_selected
+    val MyPage = R.drawable.ic_my_page
+    val MyPageSelected = R.drawable.ic_my_page_selected
+}
