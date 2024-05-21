a0a3c82bd5f5ade015cbd0e7611cb24ddf353839
jeong-1 <wjddms680@naver.com>
2024-05-22 06:02:45 +0900

    TION-40 ✨[FEAT] Compose Navigation 생성

---
+++ app/src/main/java/com/example/androidblossomingchildren/Destinations.kt
@@ -0,0 +1,24 @@
+package com.example.androidblossomingchildren
+
+enum class Destinations (
+    val route: String,
+){
+    OnBoarding(
+        route = "OnBoarding",
+    ),
+    Welcome(
+        route = "Welcome"
+    ),
+    Home(
+        route = "Home"
+    ),
+    Stamp(
+        route = "Stamp"
+    ),
+    MyPage(
+        route = "MyPage"
+    ),
+    Video(
+        route = "Video"
+    ),
+}
