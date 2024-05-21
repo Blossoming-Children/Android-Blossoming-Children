a574f1e3f9b99e2a0213ac477f5c2e36a8cdd6d0
jeong-1 <wjddms680@naver.com>
2024-05-22 06:05:07 +0900

    TION-40 ✨[FEAT] 각각의 스크린 생성

---
+++ app/src/main/java/com/example/androidblossomingchildren/ui/presentation/StampScreen.kt
@@ -0,0 +1,55 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package com.example.androidblossomingchildren.ui.presentation
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import com.example.androidblossomingchildren.Destinations
+import com.example.androidblossomingchildren.util.component.TionNavigationBar
+import com.example.androidblossomingchildren.util.component.TionNavigationBarContent
+import com.example.androidblossomingchildren.util.component.TionTopAppBar
+
+@Composable
+fun StampScreen(
+    navController: NavHostController
+) {
+    Scaffold(
+        topBar = {
+            TionTopAppBar(
+                title = {
+                    Text(text = Destinations.Stamp.route)
+                },
+            )
+        },
+        bottomBar = {
+            TionNavigationBar {
+                TionNavigationBarContent(
+                    Destinations.Stamp,
+                    navController,
+                )
+            }
+        },
+    ){
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it),
+        ) {
+            Button(onClick = { }) {
+                Text(text = "현재 화면 스탬프 화면")
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/androidblossomingchildren/ui/presentation/VideoDetailScreen.kt
@@ -0,0 +1,42 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package com.example.androidblossomingchildren.ui.presentation
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.example.androidblossomingchildren.util.component.TionTopAppBarBack
+
+@Composable
+fun VideoDetailScreen(
+    videoId: String,
+    onNavigateToBack: () -> Unit,
+) {
+    Scaffold(
+        topBar = {
+            TionTopAppBarBack(
+                title = {
+                    Text(text = "Video $videoId")
+                },
+                onNavigationClick = onNavigateToBack,
+            )
+        },
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it),
+        ) {
+
+        }
+    }
+}
