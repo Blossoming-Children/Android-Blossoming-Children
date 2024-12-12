2301bbf14f156cb0ebe4bca7bea79f734f330b0b
jeong-1 <wjddms680@naver.com>
2024-12-13 03:04:08 +0900

    [FEAT/#15] 도장판 데이터 클래스 생성

---
+++ app/src/main/java/com/example/androidblossomingchildren/libraries/network/dataClass/StampGoal.kt
@@ -0,0 +1,6 @@
+package com.example.androidblossomingchildren.libraries.network.dataClass
+
+data class StampGoal(
+    val targetStamp: Int,
+    val detail: String,
+)
---
+++ app/src/main/java/com/example/androidblossomingchildren/libraries/network/dataClass/UpdateGoalRequest.kt
@@ -0,0 +1,6 @@
+package com.example.androidblossomingchildren.libraries.network.dataClass
+
+data class UpdateGoalRequest(
+    val targetStamp: Int,
+    val goalDetail: String,
+)
