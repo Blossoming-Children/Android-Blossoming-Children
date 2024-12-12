3d41f444e3e2a89842ecbada34e4ca53328edfa1
jeong-1 <wjddms680@naver.com>
2024-12-13 03:08:32 +0900

    [FEAT/#15] 도장판 페이지 API 연결

--- app/src/main/java/com/example/androidblossomingchildren/ui/presentation/OnBoardingScreen.kt
+++ app/src/main/java/com/example/androidblossomingchildren/ui/presentation/OnBoardingScreen.kt
@@ -1,5 +1,8 @@
 package com.example.androidblossomingchildren.ui.presentation
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,33 +11,90 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.androidblossomingchildren.R
 import kotlinx.coroutines.delay
 
 @Composable
 fun OnBoardingScreen(
     onNavigateToApp: () -> Unit,
 ) {
+    val isOnBoarding = remember { mutableStateOf(true) }
+    val scale = remember { mutableFloatStateOf(0.4f) }
+    val textScale = remember { mutableFloatStateOf(0.9f) }
+    val translationY = remember { mutableFloatStateOf(0f) }
+
+    LaunchedEffect(isOnBoarding.value) {
+        if (isOnBoarding.value) {
+            // 애니메이션 설정
+            scale.value = 0.8f
+            textScale.value = 1.3f
+            translationY.value = -200f
+            delay(6000L)
+            isOnBoarding.value = false
+            onNavigateToApp()
+        }
+    }
+
     Surface {
         Column(
+            modifier = Modifier.fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center,
-            modifier = Modifier
-                .fillMaxSize(),
         ) {
-            val isOnBoarding = true
-            LaunchedEffect(isOnBoarding) {
-                delay(3000L)
-                onNavigateToApp()
-            }
+            Image(
+                painter = painterResource(id = R.drawable.img_main_logo),
+                contentDescription = "Splash Image",
+                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primary),
+                modifier = Modifier
+                    .scale(
+                        animateFloatAsState(
+                            targetValue = scale.value,
+                            animationSpec = tween(durationMillis = 6000),
+                            label = "",
+                        ).value,
+                    )
+                    .graphicsLayer(
+                        translationY = animateFloatAsState(
+                            targetValue = translationY.value,
+                            animationSpec = tween(durationMillis = 6000),
+                            label = "",
+                        ).value,
+                    ),
+            )
             Text(
-                text = "온보딩 화면",
+                text = "아이조아",
+                fontFamily = FontFamily(Font(R.font.laundrygothic_bold)),
                 color = MaterialTheme.colorScheme.primary,
-                fontSize = 50.sp,
+                fontSize = 40.sp,
                 fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .scale(
+                        animateFloatAsState(
+                            targetValue = textScale.value,
+                            animationSpec = tween(durationMillis = 6000),
+                            label = "",
+                        ).value,
+                    )
+                    .graphicsLayer(
+                        translationY = animateFloatAsState(
+                            targetValue = translationY.value,
+                            animationSpec = tween(durationMillis = 6000),
+                            label = "",
+                        ).value,
+                    ),
             )
         }
     }
--- app/src/main/java/com/example/androidblossomingchildren/ui/presentation/VideoResultScreen.kt
+++ app/src/main/java/com/example/androidblossomingchildren/ui/presentation/VideoResultScreen.kt
@@ -41,6 +41,7 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.androidblossomingchildren.R
+import com.example.androidblossomingchildren.ui.config.addStamp
 import com.example.androidblossomingchildren.ui.viewmodel.VideoDetailViewModel
 import com.example.androidblossomingchildren.util.component.TionButton
 import com.example.androidblossomingchildren.util.component.TionStampModal
@@ -50,7 +51,6 @@ import com.example.androidblossomingchildren.util.theme.Green500
 
 @Composable
 fun VideoResultScreen(
-    videoId: Int?,
     onNavigateToStamp: () -> Unit,
     onNavigateToBack: () -> Unit,
     viewModel: VideoDetailViewModel,
@@ -214,9 +214,9 @@ fun VideoResultScreen(
                 showModal.value = false // 모달이 열린 이후 다시 닫히도록 설정
                 onNavigateToStamp()
             },
-//            addStamp(
-//                userId = authId.toInt(),
-//            ),
+            addStamp(
+                userId = authId.toInt(),
+            ),
         )
     }
 }
